743d9951bbe6f7e43c7d3f1c153b0bbece1967a4
youjun <qin00045@algonquinlive.com>
2024-02-14 17:04:43 -0500

    branch test3-youjun, rename the MainActivityTest

--- app/src/androidTest/java/com/college/converter/MainActivityTestYoujun.java
+++ app/src/androidTest/java/com/college/converter/MainActivityTestYoujun.java
@@ -29,7 +29,7 @@ import org.junit.runner.RunWith;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class MainActivityTest {
+public class MainActivityTestYoujun {
 
     @Rule
     public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
