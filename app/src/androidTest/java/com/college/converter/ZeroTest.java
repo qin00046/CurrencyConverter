101a871c7d3c0336593b62947315859509965737
[Michael Hao] <xMich@LAPTOP-MU76E6PC>
2024-02-14 21:03:32 -0500

    Added a test for input 00000.00000, result is 0.00 Euros.

---
+++ app/src/androidTest/java/com/college/converter/ZeroTest.java
@@ -0,0 +1,41 @@
+package com.college.converter;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class ZeroTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void zeroTest() {
+        ViewInteraction appCompatEditText = onView(withId(R.id.entryId));
+        appCompatEditText.perform(replaceText("00000.00000"), closeSoftKeyboard());
+
+        ViewInteraction materialButton = onView(withId(R.id.convertButton));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(withId(R.id.resultId));
+        textView.check(matches(withText("0.00 Euros")));
+    }
+
+}
